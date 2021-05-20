17c8c567babc6c0737e56e8e11ba2e0dce0d695f
Théo Morfoisse <theo.morfoisse@gmail.com>
2021-05-20 09:37:44 +0200

    Update save-data.js

--- resources/save-data.js
+++ resources/save-data.js
@@ -3,6 +3,6 @@ sendData = function(uuid, user, project, data) {
     var xhr = new XMLHttpRequest();
     xhr.open("POST", "https://neurospin-data.cea.fr/experiments", true);
     xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-    //xhr.send("uuid="+uuid+"&user="+user+"&project="+project+"&data="+data);
-    xhr.send("uuid="+uuid+"&project="+project+"&data="+data);
+    xhr.send("uuid="+uuid+"&user="+user+"&project="+project+"&data="+data);
+    //xhr.send("uuid="+uuid+"&project="+project+"&data="+data);
   }
